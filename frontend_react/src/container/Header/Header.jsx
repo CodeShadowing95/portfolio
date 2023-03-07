7312d4e6752bf22b41ad4b256c1a44fccfa0514d
Frank Patrick Namegni <CodeShadowing95>
2023-03-07 09:54:07 +0100

    Header section updated

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,10 +1,77 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut"
+    }
+  }
+}
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div id="home" className="app__header app__flex">
+      <motion.div
+      // whileInView, a transition, to animate to when the element is in view.
+      // opacity from completely invisible to completely visible
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Frank Patrick</h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer Junior</p>
+            <p className="p-text">Student in ÉSTIAM-Lyon</p>
+            <p className="p-text">Looking for an (alternating) internship</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+      // whileInView, a transition, to animate to when the element is in view.
+      // opacity from completely invisible to completely visible
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+      
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
+      </motion.div>
+    </div>
   );
 }
 
--- gtp
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Update README.md"
-git push
